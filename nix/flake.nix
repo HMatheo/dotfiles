aca036a37c9de2253d7c9eb6f8947a742717cd7d
HMatheo <hersan.matheo@gmail.com>
2024-12-21 11:57:40 +0100

    Trying home-manager

--- nix/flake.nix
+++ nix/flake.nix
@@ -23,14 +23,14 @@
     # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
 
-    # home-manager, used for managing user configuration
-    #home-manager = {
-     # url = "github:nix-community/home-manager/release-24.05";
+    #home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
       # to avoid problems caused by different versions of nixpkgs dependencies.
-    #  inputs.nixpkgs.follows = "nixpkgs-darwin";
-    #};
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
 
     darwin = {
       url = "github:lnl7/nix-darwin";
@@ -70,13 +70,13 @@
         ./modules/host-users.nix
 
         # home manager
-        #home-manager.darwinModules.home-manager
-        #{
-        #  home-manager.useGlobalPkgs = true;
-        #  home-manager.useUserPackages = true;
-        #  home-manager.extraSpecialArgs = specialArgs;
-        #  home-manager.users.${username} = import ./home;
-        #}
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = specialArgs;
+          home-manager.users.${username} = import ./home;
+        }
       ];
     };
 
