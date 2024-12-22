7a33f1010cc63deba4742f817b21f76653f1268f
HMatheo <hersan.matheo@gmail.com>
2024-12-22 14:46:25 +0100

    trying yazi congig

--- nix/home/yazi.nix
+++ nix/home/yazi.nix
@@ -1,228 +1,286 @@
-{ pkgs, ... }: {
-  programs.yazi = {
-    enable = true;
-    package = pkgs.yazi;
-    extraConfig = ''
-        [manager]
-        ratio          = [ 1, 4, 3 ]
-        sort_by        = "alphabetical"
-        sort_sensitive = false
-        sort_reverse 	 = false
-        sort_dir_first = true
-        sort_translit  = false
-        linemode       = "none"
-        show_hidden    = false
-        show_symlink   = true
-        scrolloff      = 5
-        mouse_events   = [ "click", "scroll" ]
-        title_format   = "Yazi: {cwd}"
+{ config, lib, pkgs, ... }:
 
-        [preview]
-        wrap            = "no"
-        tab_size        = 2
-        max_width       = 600
-        max_height      = 900
-        cache_dir       = ""
-        image_delay     = 30
-        image_filter    = "triangle"
-        image_quality   = 75
-        sixel_fraction  = 15
-        ueberzug_scale  = 1
-        ueberzug_offset = [ 0, 0, 0, 0 ]
+with lib;
 
-        [opener]
-        edit = [
-            { run = '${EDITOR:-vi} "$@"', desc = "$EDITOR", block = true, for = "unix" },
-            { run = 'code %*',    orphan = true, desc = "code",           for = "windows" },
-            { run = 'code -w %*', block = true,  desc = "code (block)",   for = "windows" },
-        ]
-        open = [
-            { run = 'xdg-open "$1"',                desc = "Open", for = "linux" },
-            { run = 'open "$@"',                    desc = "Open", for = "macos" },
-            { run = 'start "" "%1"', orphan = true, desc = "Open", for = "windows" },
-        ]
-        reveal = [
-            { run = 'xdg-open "$(dirname "$1")"',           desc = "Reveal", for = "linux" },
-            { run = 'open -R "$1"',                         desc = "Reveal", for = "macos" },
-            { run = 'explorer /select,"%1"', orphan = true, desc = "Reveal", for = "windows" },
-            { run = '''exiftool "$1"; echo "Press enter to exit"; read _''', block = true, desc = "Show EXIF", for = "unix" },
-        ]
-        extract = [
-            { run = 'ya pub extract --list "$@"', desc = "Extract here", for = "unix" },
-            { run = 'ya pub extract --list %*',   desc = "Extract here", for = "windows" },
-        ]
-        play = [
-            { run = 'mpv --force-window "$@"', orphan = true, for = "unix" },
-            { run = 'mpv --force-window %*', orphan = true, for = "windows" },
-            { run = '''mediainfo "$1"; echo "Press enter to exit"; read _''', block = true, desc = "Show media info", for = "unix" },
-        ]
+let
+  cfg = config.programs.yazi;
+  tomlFormat = pkgs.formats.toml { };
 
-        [open]
-        rules = [
-            # Folder
-            { name = "*/", use = [ "edit", "open", "reveal" ] },
-            # Text
-            { mime = "text/*", use = [ "edit", "reveal" ] },
-            # Image
-            { mime = "image/*", use = [ "open", "reveal" ] },
-            # Media
-            { mime = "{audio,video}/*", use = [ "play", "reveal" ] },
-            # Archive
-            { mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}", use = [ "extract", "reveal" ] },
-            # JSON
-            { mime = "application/{json,ndjson}", use = [ "edit", "reveal" ] },
-            { mime = "*/javascript", use = [ "edit", "reveal" ] },
-            # Empty file
-            { mime = "inode/empty", use = [ "edit", "reveal" ] },
-            # Fallback
-            { name = "*", use = [ "open", "reveal" ] },
-        ]
+  bashIntegration = ''
+    function ${cfg.shellWrapperName}() {
+      local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+      yazi "$@" --cwd-file="$tmp"
+      if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+      fi
+      rm -f -- "$tmp"
+    }
+  '';
 
-        [tasks]
-        micro_workers    = 10
-        macro_workers    = 10
-        bizarre_retry    = 3
-        image_alloc      = 536870912  # 512MB
-        image_bound      = [ 0, 0 ]
-        suppress_preload = false
+  fishIntegration = ''
+    function ${cfg.shellWrapperName}
+      set tmp (mktemp -t "yazi-cwd.XXXXX")
+      yazi $argv --cwd-file="$tmp"
+      if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+      end
+      rm -f -- "$tmp"
+    end
+  '';
 
-        [plugin]
+  nushellIntegration = ''
+    def --env ${cfg.shellWrapperName} [...args] {
+      let tmp = (mktemp -t "yazi-cwd.XXXXX")
+      yazi ...$args --cwd-file $tmp
+      let cwd = (open $tmp)
+      if $cwd != "" and $cwd != $env.PWD {
+        cd $cwd
+      }
+      rm -fp $tmp
+    }
+  '';
+in {
+  meta.maintainers = with maintainers; [ xyenon eljamm ];
 
-        fetchers = [
-            # Mimetype
-            { id = "mime", name = "*", run = "mime", if = "!mime", prio = "high" },
-        ]
-        spotters = [
-            { name = "*/", run = "folder" },
-            # Code
-            { mime = "text/*", run = "code" },
-            { mime = "*/{xml,javascript,wine-extension-ini}", run = "code" },
-            # Image
-            { mime = "image/{avif,hei?,jxl,svg+xml}", run = "magick" },
-            { mime = "image/*", run = "image" },
-            # Video
-            { mime = "video/*", run = "video" },
-            # Fallback
-            { name = "*", run = "file" },
-        ]
-        preloaders = [
-            # Image
-            { mime = "image/{avif,hei?,jxl,svg+xml}", run = "magick" },
-            { mime = "image/*", run = "image" },
-            # Video
-            { mime = "video/*", run = "video" },
-            # PDF
-            { mime = "application/pdf", run = "pdf" },
-            # Font
-            { mime = "font/*", run = "font" },
-            { mime = "application/ms-opentype", run = "font" },
-        ]
-        previewers = [
-            { name = "*/", run = "folder", sync = true },
-            # Code
-            { mime = "text/*", run = "code" },
-            { mime = "*/{xml,javascript,wine-extension-ini}", run = "code" },
-            # JSON
-            { mime = "application/{json,ndjson}", run = "json" },
-            # Image
-            { mime = "image/{avif,hei?,jxl,svg+xml}", run = "magick" },
-            { mime = "image/*", run = "image" },
-            # Video
-            { mime = "video/*", run = "video" },
-            # PDF
-            { mime = "application/pdf", run = "pdf" },
-            # Archive
-            { mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}", run = "archive" },
-            { mime = "application/{debian*-package,redhat-package-manager,rpm,android.package-archive}", run = "archive" },
-            { name = "*.{AppImage,appimage}", run = "archive" },
-            # Virtual Disk / Disk Image
-            { mime = "application/{iso9660-image,qemu-disk,ms-wim,apple-diskimage}", run = "archive" },
-            { mime = "application/virtualbox-{vhd,vhdx}", run = "archive" },
-            { name = "*.{img,fat,ext,ext2,ext3,ext4,squashfs,ntfs,hfs,hfsx}", run = "archive" },
-            # Font
-            { mime = "font/*", run = "font" },
-            { mime = "application/ms-opentype", run = "font" },
-            # Empty file
-            { mime = "inode/empty", run = "empty" },
-            # Fallback
-            { name = "*", run = "file" },
-        ]
+  options.programs.yazi = {
+    enable = mkEnableOption "yazi";
 
-        [input]
-        cursor_blink = false
+    package = mkPackageOption pkgs "yazi" { };
 
-        # cd
-        cd_title  = "Change directory:"
-        cd_origin = "top-center"
-        cd_offset = [ 0, 2, 50, 3 ]
+    shellWrapperName = mkOption {
+      type = types.str;
+      default = "yy";
+      example = "y";
+      description = ''
+        Name of the shell wrapper to be called.
+      '';
+    };
 
-        # create
-        create_title  = [ "Create:", "Create (dir):" ]
-        create_origin = "top-center"
-        create_offset = [ 0, 2, 50, 3 ]
+    enableBashIntegration = mkEnableOption "Bash integration" // {
+      default = true;
+    };
 
-        # rename
-        rename_title  = "Rename:"
-        rename_origin = "hovered"
-        rename_offset = [ 0, 1, 50, 3 ]
+    enableZshIntegration = mkEnableOption "Zsh integration" // {
+      default = true;
+    };
 
-        # filter
-        filter_title  = "Filter:"
-        filter_origin = "top-center"
-        filter_offset = [ 0, 2, 50, 3 ]
+    enableFishIntegration = mkEnableOption "Fish integration" // {
+      default = true;
+    };
 
-        # find
-        find_title  = [ "Find next:", "Find previous:" ]
-        find_origin = "top-center"
-        find_offset = [ 0, 2, 50, 3 ]
+    enableNushellIntegration = mkEnableOption "Nushell integration" // {
+      default = true;
+    };
 
-        # search
-        search_title  = "Search via {n}:"
-        search_origin = "top-center"
-        search_offset = [ 0, 2, 50, 3 ]
+    keymap = mkOption {
+      type = tomlFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          input.prepend_keymap = [
+            { run = "close"; on = [ "<C-q>" ]; }
+            { run = "close --submit"; on = [ "<Enter>" ]; }
+            { run = "escape"; on = [ "<Esc>" ]; }
+            { run = "backspace"; on = [ "<Backspace>" ]; }
+          ];
+          manager.prepend_keymap = [
+            { run = "escape"; on = [ "<Esc>" ]; }
+            { run = "quit"; on = [ "q" ]; }
+            { run = "close"; on = [ "<C-q>" ]; }
+          ];
+        }
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/yazi/keymap.toml`.
 
-        # shell
-        shell_title  = [ "Shell:", "Shell (block):" ]
-        shell_origin = "top-center"
-        shell_offset = [ 0, 2, 50, 3 ]
+        See <https://yazi-rs.github.io/docs/configuration/keymap>
+        for the full list of options.
+      '';
+    };
 
-        [confirm]
-        # trash
-        trash_title 	= "Trash {n} selected file{s}?"
-        trash_origin	= "center"
-        trash_offset	= [ 0, 0, 70, 20 ]
+    settings = mkOption {
+      type = tomlFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          log = {
+            enabled = false;
+          };
+          manager = {
+            show_hidden = false;
+            sort_by = "mtime";
+            sort_dir_first = true;
+            sort_reverse = true;
+          };
+        }
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/yazi/yazi.toml`.
 
-        # delete
-        delete_title 	= "Permanently delete {n} selected file{s}?"
-        delete_origin	= "center"
-        delete_offset	= [ 0, 0, 70, 20 ]
+        See <https://yazi-rs.github.io/docs/configuration/yazi>
+        for the full list of options.
+      '';
+    };
 
-        # overwrite
-        overwrite_title   = "Overwrite file?"
-        overwrite_content = "Will overwrite the following file:"
-        overwrite_origin  = "center"
-        overwrite_offset  = [ 0, 0, 50, 15 ]
+    theme = mkOption {
+      type = tomlFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          filetype = {
+            rules = [
+              { fg = "#7AD9E5"; mime = "image/*"; }
+              { fg = "#F3D398"; mime = "video/*"; }
+              { fg = "#F3D398"; mime = "audio/*"; }
+              { fg = "#CD9EFC"; mime = "application/bzip"; }
+            ];
+          };
+        }
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/yazi/theme.toml`.
 
-        # quit
-        quit_title   = "Quit?"
-        quit_content = "The following task is still running, are you sure you want to quit?"
-        quit_origin  = "center"
-        quit_offset  = [ 0, 0, 50, 15 ]
+        See <https://yazi-rs.github.io/docs/configuration/theme>
+        for the full list of options
+      '';
+    };
 
-        [pick]
-        open_title  = "Open with:"
-        open_origin = "hovered"
-        open_offset = [ 0, 1, 50, 7 ]
+    initLua = mkOption {
+      type = with types; nullOr (either path lines);
+      default = null;
+      description = ''
+        The init.lua for Yazi itself.
+      '';
+      example = literalExpression "./init.lua";
+    };
 
-        [which]
-        sort_by      	 = "none"
-        sort_sensitive = false
-        sort_reverse 	 = false
-        sort_translit  = false
-    '';
+    plugins = mkOption {
+      type = with types; attrsOf (oneOf [ path package ]);
+      default = { };
+      description = ''
+        Lua plugins.
+        Values should be a package or path containing an `init.lua` file.
+        Will be linked to {file}`$XDG_CONFIG_HOME/yazi/plugins/<name>.yazi`.
+
+        See <https://yazi-rs.github.io/docs/plugins/overview>
+        for documentation.
+      '';
+      example = literalExpression ''
+        {
+          foo = ./foo;
+          bar = pkgs.bar;
+        }
+      '';
+    };
+
+    flavors = mkOption {
+      type = with types; attrsOf (oneOf [ path package ]);
+      default = { };
+      description = ''
+        Pre-made themes.
+        Values should be a package or path containing the required files.
+        Will be linked to {file}`$XDG_CONFIG_HOME/yazi/flavors/<name>.yazi`.
+
+        See <https://yazi-rs.github.io/docs/flavors/overview/> for documentation.
+      '';
+      example = literalExpression ''
+        {
+          foo = ./foo;
+          bar = pkgs.bar;
+        }
+      '';
+    };
   };
 
-  home.sessionVariables = {
-    YAZI_CONFIG = "${config.home.homeDirectory}/.config/yazi/config.toml"; # Exemple pour un fichier de config perso
+  config = mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    programs.bash.initExtra = mkIf cfg.enableBashIntegration bashIntegration;
+
+    programs.zsh.initExtra = mkIf cfg.enableZshIntegration bashIntegration;
+
+    programs.fish.interactiveShellInit =
+      mkIf cfg.enableFishIntegration fishIntegration;
+
+    programs.nushell.extraConfig =
+      mkIf cfg.enableNushellIntegration nushellIntegration;
+
+    xdg.configFile = {
+      "yazi/keymap.toml" = mkIf (cfg.keymap != { }) {
+        source = tomlFormat.generate "yazi-keymap" cfg.keymap;
+      };
+      "yazi/yazi.toml" = mkIf (cfg.settings != { }) {
+        source = tomlFormat.generate "yazi-settings" cfg.settings;
+      };
+      "yazi/theme.toml" = mkIf (cfg.theme != { }) {
+        source = tomlFormat.generate "yazi-theme" cfg.theme;
+      };
+      "yazi/init.lua" = mkIf (cfg.initLua != null)
+        (if builtins.isPath cfg.initLua then {
+          source = cfg.initLua;
+        } else {
+          text = cfg.initLua;
+        });
+    } // (mapAttrs' (name: value:
+      nameValuePair "yazi/flavors/${name}.yazi" { source = value; })
+      cfg.flavors) // (mapAttrs' (name: value:
+        nameValuePair "yazi/plugins/${name}.yazi" { source = value; })
+        cfg.plugins);
+
+    warnings = filter (s: s != "") (concatLists [
+      (mapAttrsToList (name: value:
+        optionalString (hasSuffix ".yazi" name) ''
+          Flavors like `programs.yazi.flavors."${name}"` should no longer have the suffix ".yazi" in their attribute name.
+          The flavor will be linked to `$XDG_CONFIG_HOME/yazi/flavors/${name}.yazi`.
+          You probably want to rename it to `programs.yazi.flavors."${
+            removeSuffix ".yazi" name
+          }"`.
+        '') cfg.flavors)
+      (mapAttrsToList (name: value:
+        optionalString (hasSuffix ".yazi" name) ''
+          Plugins like `programs.yazi.plugins."${name}"` should no longer have the suffix ".yazi" in their attribute name.
+          The plugin will be linked to `$XDG_CONFIG_HOME/yazi/plugins/${name}.yazi`.
+          You probably want to rename it to `programs.yazi.plugins."${
+            removeSuffix ".yazi" name
+          }"`.
+        '') cfg.plugins)
+    ]);
+
+    assertions = let
+      mkAsserts = opt: requiredFiles:
+        mapAttrsToList (name: value:
+          let
+            isDir = pathIsDirectory "${value}";
+            msgNotDir = optionalString (!isDir)
+              "The path or package should be a directory, not a single file.";
+            isFileMissing = file:
+              !(pathExists "${value}/${file}")
+              || pathIsDirectory "${value}/${file}";
+            missingFiles = filter isFileMissing requiredFiles;
+            msgFilesMissing = optionalString (missingFiles != [ ])
+              "The ${singularOpt} is missing these files: ${
+                toString missingFiles
+              }";
+            singularOpt = removeSuffix "s" opt;
+          in {
+            assertion = isDir && missingFiles == [ ];
+            message = ''
+              Value at `programs.yazi.${opt}.${name}` is not a valid yazi ${singularOpt}.
+              ${msgNotDir}
+              ${msgFilesMissing}
+              Evaluated value: `${value}`
+            '';
+          }) cfg.${opt};
+    in (mkAsserts "flavors" [
+      "flavor.toml"
+      "tmtheme.xml"
+      "README.md"
+      "preview.png"
+      "LICENSE"
+      "LICENSE-tmtheme"
+    ]) ++ (mkAsserts "plugins" [ "init.lua" ]);
   };
 }
