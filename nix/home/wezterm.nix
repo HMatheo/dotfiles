0a0296361742ad4f894a2a9cc3d5bf74c4de14af
HMatheo <hersan.matheo@gmail.com>
2024-12-22 22:56:36 +0100

    trying things

--- nix/home/wezterm.nix
+++ nix/home/wezterm.nix
@@ -1,6 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+
+homeManagerConfiguration = {
+  # Installation de wezterm
   programs.wezterm = {
-    enable = true;
-    package = pkgs.wezterm;
+    enable = true; # Active wezterm via Home Manager
   };
+
+  # Génération du fichier de configuration wezterm.lua
+  home.file.".config/wezterm/wezterm.lua".text = ''
+    local wezterm = require 'wezterm'
+
+    return {
+      font = wezterm.font("FiraCode Nerd Font"), -- Police
+      color_scheme = "Dracula",                 -- Thème de couleurs
+      enable_tab_bar = false,                   -- Désactive la barre d'onglets
+    }
+  '';
 };
