b1873b429838742612bc73d017ee08519da0998f
HMatheo <hersan.matheo@gmail.com>
2024-12-27 13:33:51 +0100

    removed eza (using nushell)

--- nix/home/core.nix
+++ nix/home/core.nix
@@ -30,15 +30,6 @@
       vimAlias = true;
     };
 
-    # A modern replacement for ‘ls’
-    # useful in bash/zsh prompt, not in nushell.
-    eza = {
-      enable = true;
-      git = true;
-      icons = true;
-      enableZshIntegration = true;
-    };
-
     # terminal file manager
     yazi = {
       enable = true;
